d86505311e9f3fc6b8d7710527986fe296af0a51
ITigor1985 <xromer85@gmail.com>
2021-12-01 11:10:50 +0200

    123

---
+++ src/js/gallery.js
@@ -0,0 +1,11 @@
+import {slick} from 'slick-carousel/slick'
+
+
+  $(document).ready(function () {
+    
+   
+      $('.gallery-item').slick({
+        dots: true,
+        arrows: true,
+      });
+  });
