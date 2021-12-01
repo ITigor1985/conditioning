214c8e6f158a5427a0c8c56d82cd9080d28cb37e
ITigor1985 <81867285+ITigor1985@users.noreply.github.com>
2021-12-01 13:48:21 +0000

    Deploying to gh-pages from @ ITigor1985/conditioning@d37ae2db74e406ee3880fa67853f58662c16b446 🚀

---
+++ index.5d64c127.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".gallery__list").slick({arrows:!0})}));
+//# sourceMappingURL=index.5d64c127.js.map
--- index.a5a457bd.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".gallery__item").slick({dots:!0,arrows:!0})}));
-//# sourceMappingURL=index.a5a457bd.js.map
