dfd52c573f1326bd68cc5912ac90c869e80fa057
ITigor1985 <xromer85@gmail.com>
2021-12-03 15:22:11 +0200

    123

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,19 +1,43 @@
 const refs ={
-    btnOpen: document.querySelectorAll('.open'),
-    btnClose: document.querySelectorAll('.close'),
-    divAnswer:document.querySelectorAll('.answer')
+    ulEl: document.querySelector('.faq__list'),
+    btnsOpen: document.querySelectorAll('.open'),
+    btnsClose: document.querySelectorAll('.close'),
+    divAnswers:document.querySelectorAll('.answer')
 }
 
-refs.btnOpen.addEventListener('click', openAnswer);
-function openAnswer(){
-    refs.btnOpen.classList.add('is-hidden');
-    refs.btnClose.classList.remove('is-hidden');
-    refs.divAnswer.classList.remove('is-hidden');
-}
 
-refs.btnClose.addEventListener('click', closeAnswer);
-function closeAnswer(){
-    refs.btnClose.classList.add('is-hidden');
-    refs.btnOpen.classList.remove('is-hidden');
-    refs.divAnswer.classList.add('is-hidden');
-}
+refs.btnsOpen.forEach(btnOpen => {
+    btnOpen.addEventListener('click', ()=>{
+        btnOpen.classList.toggle('is-hidden')
+    });
+})
+// refs.ulEl.addEventListener('click', selectBtn);
+// function selectBtn(event){
+//     console.log(refs.btnOpen)
+//     if (event.target.nodeName !== "BUTTON") {
+//         console.log(event.target);
+//         return;
+//       }
+//       if(event.target.classList.contains('open')){
+//         event.target.classList.add('is-hidden');
+//         refs.btnClose.classList.remove('is-hidden');
+//         refs.divAnswer.classList.remove('is-hidden');
+//       }else{
+//         event.target.classList.add('is-hidden');
+//         refs.btnOpen.classList.remove('is-hidden');
+//         refs.divAnswer.classList.add('is-hidden');
+//       }
+        
+    
+// }
+
+// refs.ulEl.addEventListener('click', closeAnswer);
+// function closeAnswer(event){
+//     console.log("asd",event.target)
+//     if (event.target !== "BUTTON") {
+//         return;
+//       }
+//       event.target.classList.add('is-hidden');
+//     refs.btnOpen.classList.remove('is-hidden');
+//     refs.divAnswer.classList.add('is-hidden');
+// }
