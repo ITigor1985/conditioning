cee8c42a795da5eddadb23eea3d56c23cecb771a
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-11 11:16:36 +0200

    123

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,43 +1,10 @@
-const refs ={
-    ulEl: document.querySelector('.faq__list'),
-    btnsOpen: document.querySelectorAll('.open'),
-    btnsClose: document.querySelectorAll('.close'),
-    divAnswers:document.querySelectorAll('.answer')
-}
 
 
-refs.btnsOpen.forEach(btnOpen => {
-    btnOpen.addEventListener('click', ()=>{
-        btnOpen.classList.toggle('is-hidden')
-    });
-})
-// refs.ulEl.addEventListener('click', selectBtn);
-// function selectBtn(event){
-//     console.log(refs.btnOpen)
-//     if (event.target.nodeName !== "BUTTON") {
-//         console.log(event.target);
-//         return;
-//       }
-//       if(event.target.classList.contains('open')){
-//         event.target.classList.add('is-hidden');
-//         refs.btnClose.classList.remove('is-hidden');
-//         refs.divAnswer.classList.remove('is-hidden');
-//       }else{
-//         event.target.classList.add('is-hidden');
-//         refs.btnOpen.classList.remove('is-hidden');
-//         refs.divAnswer.classList.add('is-hidden');
-//       }
-        
-    
-// }
+import Accordion from 'accordion/src/accordion.mjs'
+import 'accordion/src/accordion.css'
 
-// refs.ulEl.addEventListener('click', closeAnswer);
-// function closeAnswer(event){
-//     console.log("asd",event.target)
-//     if (event.target !== "BUTTON") {
-//         return;
-//       }
-//       event.target.classList.add('is-hidden');
-//     refs.btnOpen.classList.remove('is-hidden');
-//     refs.divAnswer.classList.add('is-hidden');
-// }
+var el = document.querySelector(".accordion");
+new Accordion(el);
+
+
+
