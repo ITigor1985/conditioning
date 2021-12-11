099f4c23730d883071236fdca97cbb73f419e12f
ITigor1985 <81867285+ITigor1985@users.noreply.github.com>
2021-12-11 09:17:35 +0000

    Deploying to gh-pages from @ ITigor1985/conditioning@cee8c42a795da5eddadb23eea3d56c23cecb771a 🚀

--- index.4aa2b173.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".faq__list"),document.querySelectorAll(".open"),document.querySelectorAll(".close"),document.querySelectorAll(".answer")][1].forEach((function(e){e.addEventListener("click",(function(){e.classList.toggle("is-hidden")}))}));
-//# sourceMappingURL=index.4aa2b173.js.map
--- index.91ebfad4.js
+++
@@ -1,2 +0,0 @@
-({ulEl:document.querySelector(".faq__list"),btnsOpen:document.querySelectorAll(".open"),btnsClose:document.querySelectorAll(".close"),divAnswers:document.querySelectorAll(".answer")}).btnsOpen.forEach((e=>{e.addEventListener("click",(()=>{e.classList.toggle("is-hidden")}))}));
-//# sourceMappingURL=index.91ebfad4.js.map
