a8066a0dc0aaa46a2cb8314b957e3058f8468852
ITigor1985 <81867285+ITigor1985@users.noreply.github.com>
2021-12-13 07:57:02 +0000

    Deploying to gh-pages from @ ITigor1985/conditioning@8a8a750cde977982c6526088765f17d5bb7a0d3b 🚀

---
+++ index.0c526c78.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".faq").forEach((e=>{e.addEventListener("click",(function(){e.classList.toggle("active")}))}));
+//# sourceMappingURL=index.0c526c78.js.map
---
+++ index.38167a6b.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".faq").forEach((function(c){c.addEventListener("click",(function(){c.classList.toggle("active")}))}));
+//# sourceMappingURL=index.38167a6b.js.map
