0a0a0dfbe71bd56b1c46003844ed80e0900a515f
ITigor1985 <xromer85@gmail.com>
2021-12-17 15:23:27 +0200

    123

---
+++ src/js/cleaner.js
@@ -0,0 +1,7 @@
+const cleanerLink = document.querySelector('.cond-nav-link-service');
+
+cleanerLink.addEventListener('click', linkPreventDefault)
+
+function linkPreventDefault(event){
+    event.preventDefault();
+}
